a06b3912966d71789cc744281e066ae0ea1502e9
nikita <nikitakhandare50@gmail.com>
2021-06-05 20:49:22 -0800

    initial commit

---
+++ assign1_factorial.py
@@ -0,0 +1,21 @@
+#write a program for factorial with or without recursion
+# i=int(input("enter number:"))
+# fac=1
+# while(i>=0):
+#     fac=fac*i
+#     i=i-1
+#     print("factorial=",fac)
+
+#factorial program in python using recursion
+def fact(n):
+    if n==1:
+        return 1
+    else:
+        return(n*fact(n-1))
+
+
+n=int(input("enter number:"))
+z=fact(n)
+print("factorial=",z)
+
+
